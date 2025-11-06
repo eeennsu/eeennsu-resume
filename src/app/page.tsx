3c9426d34047d7ffb24978f4556b9afe4c5f34e4
eeennsu <diso592@naver.com>
2025-11-06 21:31:27 +0900

    feature: custom

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import useGetIp from '@shared/hooks/useGetIp';
 import { FC } from 'react';
 
 import CertificationsWidget from '@widgets/Certification';
@@ -9,10 +10,8 @@ import ProfileWidget from '@widgets/Profile';
 import SkillsWidget from '@widgets/Skill';
 
 const HomePage: FC = async () => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL || ''}/api/ip`);
-  const data = await response.json();
-
-  console.log('data', data);
+  const { ip } = useGetIp();
+  console.log('ip', ip);
 
   return (
     <main className='flex flex-col gap-10 md:gap-16'>
